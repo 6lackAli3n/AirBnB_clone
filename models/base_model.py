655c52ae455207d22cc3682e3e85762e4fb4ea0f
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-24 05:42:54 +0100

    json

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
     """Defines all common attributes/methods for other classes."""
 
@@ -13,7 +14,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime
+                            (value, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif key != '__class__':
                     setattr(self, key, value)
         else:
@@ -22,7 +24,8 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format
+    (self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at with
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,11 @@
 import json
 import os
 from models.base_model import BaseModel
+
+
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
+    """Serializes instances to a JSON
+    file and deserializes JSON file to instances."""
     __file_path = "file.json"
     __objects = {}
 
