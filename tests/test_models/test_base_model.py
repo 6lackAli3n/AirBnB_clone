54c032f39e438cda9d5fe896f68b6ce869e23142
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-24 05:42:25 +0100

    json

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(self.base_model, 'updated_at'))
 
     def test_str_method(self):
-        expected_str = "[BaseModel] ({}) {}".format(self.base_model.id, self.base_model.__dict__)
+        expected_str = "[BaseModel] ({}) {}".format
+        (self.base_model.id, self.base_model.__dict__)
         self.assertEqual(str(self.base_model), expected_str)
 
     def test_save_method(self):
@@ -28,14 +29,21 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(isinstance(base_model_dict, dict))
         self.assertEqual(base_model_dict['__class__'], 'BaseModel')
         self.assertEqual(base_model_dict['id'], self.base_model.id)
-        self.assertEqual(base_model_dict['created_at'], self.base_model.created_at.isoformat())
-        self.assertEqual(base_model_dict['updated_at'], self.base_model.updated_at.isoformat())
+        self.assertEqual
+        (base_model_dict['created_at'], self.base_model.created_at.isoformat())
+        self.assertEqual
+        (base_model_dict['updated_at'], self.base_model.updated_at.isoformat())
 
     def test_init_method(self):
-        base_model2 = BaseModel(id="123", created_at="2023-01-01T12:00:00.000000", updated_at="2023-01-01T12:00:00.000000")
+        base_model2 = BaseModel
+        (
+                id="123", created_at="2023-01-01T12:00:00.000000",
+                updated_at="2023-01-01T12:00:00.000000")
         self.assertEqual(base_model2.id, "123")
-        self.assertEqual(base_model2.created_at, datetime.datetime(2023, 1, 1, 12, 0))
-        self.assertEqual(base_model2.updated_at, datetime.datetime(2023, 1, 1, 12, 0))
+        self.assertEqual
+        (base_model2.created_at, datetime.datetime(2023, 1, 1, 12, 0))
+        self.assertEqual
+        (base_model2.updated_at, datetime.datetime(2023, 1, 1, 12, 0))
 
     def tearDown(self):
         del self.base_model
