19c3d82ac688a0fe5fd9b1ca422f0be78a5edfce
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-24 22:51:32 +0100

    json

--- models/base_model.py
+++ models/base_model.py
@@ -24,8 +24,8 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format
-    (self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates the public instance attribute updated_at with
