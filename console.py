b75790f68b53e8ddecfe84b23de794d3b96d9633
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-25 05:04:04 +0100

    console

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 This module defines the console for the AirBnB clone project.
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,6 +14,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         ''' exit the program '''
+        print("")
         return True
 
     def help_EOF(self):
@@ -29,6 +32,113 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         ''' do nothing with empty line '''
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            print(obj)
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            del storage.all()[key]
+            storage.save()
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        objs = []
+        if not arg:
+            for obj in storage.all().values():
+                objs.append(str(obj))
+            print(objs)
+            return
+        try:
+            class_name = arg.split()[0]
+            objs = [str(obj) for obj in storage.all().values()
+                    if obj.__class__.__name__ == arg]
+            if not objs:
+                print("** no instance found **")
+                return
+        except keyError:
+            print("** class doesn't exist **")
+            return
+        print(objs)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            class_name = args[0]
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(class_name, obj_id)
+            obj = storage.all().get(key)
+            if not obj:
+                print("** no instance found **")
+                return
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            attr_name = args[2]
+            attr_value = args[3]
+            if hasattr(obj, attr_name):
+                attr_type = type(getattr(obj, attr_name))
+                setattr(obj, attr_name, attr_type(attr_value))
+                storage.save()
+        except NameError:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
