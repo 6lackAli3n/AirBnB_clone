db9d4ffb22b16a731aa5992e73f37ecf87c61a02
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-25 15:46:57 +0100

    tests

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """Test cases for State class."""
+
+    def test_attributes(self):
+        """Test State attributes."""
+        state = State()
+        self.assertEqual(state.name, "")
+
+if __name__ == "__main__":
+    unittest.main()
